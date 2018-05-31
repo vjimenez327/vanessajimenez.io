71c6fcfafd3e9b1b48e619f7a4f16bbebf936b4e
Vanessa Jiménez <16891417+vjimenez327@users.noreply.github.com>
2018-05-30 21:23:16 -0400

    working link

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import styled from 'styled-components'
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 
 const HeaderDiv = styled.div`
