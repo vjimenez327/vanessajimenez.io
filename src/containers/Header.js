603847492cfbee7473c58697da2e6e9366b6ca43
Vanessa Jimenez <vjimenez127@gmail.com>
2018-05-30 21:52:38 -0400

    updated header with Navlinks

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
-
 const HeaderDiv = styled.div`
 padding-top: 5vh;
 text-align: center;
 font-family: 'Zilla Slab Highlight', sans-serif;
 color: blue;
 `;
+
+const ProfileTitle = styled.h1`
+  color: black;
+`
 
 const NavLink = styled(Link)`
   display: inline-flex;
@@ -22,11 +25,20 @@ class Header extends Component {
   render(){
     return (
       <HeaderDiv>
-      Hi I'm Vanessa!
+        <Link to="/">
+         <ProfileTitle>VANESSA JIMÉNEZ</ProfileTitle>
+        </Link>
+
       <div>
+        <NavLink to='/'>ABOUT</NavLink>
+        <NavLink to='/code'>CODE</NavLink>
+        <NavLink to='/life'>LIFE</NavLink>
+        <NavLink to='/life'>RÈSUMÈ</NavLink>
+        <NavLink to='/contact'>CONTACT</NavLink>
       </div>
+      <hr/>
       </HeaderDiv>
-    )
+    );
   }
 }
 
